f8c57d5fe10998fcee3739268c7d763e1414b05c
RAMSES-666 <72388998+RAMSES-666@users.noreply.github.com>
2021-04-27 18:15:04 +0200

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FestivalController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('Acceuil');
+});
+
+Route::get('GestionEtablissement', [FestivalController::class, 'obtenirReqEtablissements']);
+
+Route::get('Detail', [FestivalController::class, 'obtenirDetailEtablissement']);
+
+
+Route::get('Creation', function () {
+    return view('creationEtablissement');
+});
+
+Route::post('CreationExec', [FestivalController::class, 'CreaEtab']);
+
+Route::get('Modification', [FestivalController::class, 'obtenirModifEtablissement']);
+Route::get('ModificationA', [FestivalController::class, 'obtenirModifAttribution']);
+
+Route::post('ModificationExec', [FestivalController::class, 'ModifEtab']);
+
+Route::get('Suppresion', [FestivalController::class, 'afficheSuppression']);
+
+Route::get('SuppressionExec', [FestivalController::class, 'suppression']);
+
+Route::get('AttributionChambres', [FestivalController::class, 'obtenirReqEtablissementsOffrantChambres']);
+
+Route::get('EffectuerOuModifiertest', [FestivalController::class, 'afficheAttribution']);
+
+Route::get('EffectuerOuModifier', function () {
+    return view('Attribution');
+});
+
+Route::post('EffectAttrib', [FestivalController::class, 'AffectAtrib']);
+
+
+// Route::get('AttributionChambres', function () {
+//     return view('consultationAttributions');
+// });
